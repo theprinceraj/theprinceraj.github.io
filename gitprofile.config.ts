e867a4b38b332e8b9958c9e91a9423830fb161d5
Prince Raj <profile.princeraj@gmail.com>
2024-05-04 00:37:23 +0530

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -13,7 +13,7 @@ const CONFIG = {
   projects: {
     github: {
       display: true, // Display GitHub projects?
-      header: 'Github Projects',
+      header: 'My Projects',
       mode: 'automatic', // Mode can be: 'automatic' or 'manual'
       automatic: {
         sortBy: 'stars', // Sort projects by 'stars' or 'updated'
@@ -29,7 +29,7 @@ const CONFIG = {
       },
     },
     external: {
-      header: 'My Projects',
+      /* header: 'My Projects',
       // To hide the `External Projects` section, keep it empty.
       projects: [
         {
@@ -49,7 +49,7 @@ const CONFIG = {
           link: 'https://example.com',
         },
       ],
-    },
+    },*/
   },
   seo: {
     title: 'Prince Raj',
