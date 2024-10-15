7353bfdb0fa03015cc5a8f54c6d964a90ec43b6d
theprinceraj <profile.princeraj@gmail.com>
2024-10-16 03:32:56 +0530

    .

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,9 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import GitProfile from './components/gitprofile.tsx';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <GitProfile config={CONFIG} />,
-  },
-  {
-    path: '/cv',
-    loader: () => (window.location.href = '/resume.pdf'),
-  },
-]);
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <GitProfile config={CONFIG} />
+  </React.StrictMode>,
 );
