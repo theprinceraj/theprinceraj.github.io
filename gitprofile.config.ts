47f7e206c025370dacb3308071397cda66ef6007
Prince Raj <profile.princeraj@gmail.com>
2024-10-16 01:09:25 +0530

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -25,7 +25,7 @@ const CONFIG = {
       },
       manual: {
         // Properties for manually specifying projects
-        projects: ['theprinceraj/Profile-Cards-API', 'theprinceraj/Markshify'], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
+        projects: [], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
       },
     },
     external: {
