0d7ee32c319eb8fbb2a9db9871a3365969564f72
theprinceraj <profile.princeraj@gmail.com>
2024-10-16 03:26:17 +0530

    "Updated dependencies, added react-router-dom, and modified main.tsx to use react-router-dom; also updated tsconfig.json and tsconfig.node.json"

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import GitProfile from './components/gitprofile.tsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <GitProfile config={CONFIG} />,
+  },
+  {
+    path: '/cv',
+    loader: () => (window.location.href = '/resume.pdf'),
+  },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <GitProfile config={CONFIG} />
-  </React.StrictMode>,
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
 );
