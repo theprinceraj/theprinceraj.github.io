fd90bf2f30cdeca2e10a51cedadc96244f0bdacf
theprinceraj <profile.princeraj@gmail.com>
2026-05-21 03:06:43 +0530

    feat: update Vercel SVG icon, refactor ToolboxItemsRow to use direct component rendering, and adjust animation durations in AboutSection

--- src/components/ToolboxItemsRow.tsx
+++ src/components/ToolboxItemsRow.tsx
@@ -1,5 +1,4 @@
 import { twMerge } from "tailwind-merge";
-import { ToolIcon } from "./ToolIcon";
 import { Fragment } from "react";
 
 interface toolboxItem {
@@ -25,14 +24,18 @@ export const ToolboxItemsRow = ({
             <div className={twMerge("flex flex-none py-0.5 gap-6 pr-3", itemsWrapperClassName)}>
                 {[...new Array(2)].fill(0).map((_, index) => (
                     <Fragment key={index}>
-                        {items.map((item) => (
-                            <div
-                                key={item.title}
-                                className="inline-flex items-center gap-4 py-2 px-3 outline outline-2 outline-white/10 rounded-lg">
-                                <ToolIcon component={item.iconType} />
-                                <span className="font-semibold">{item.title}</span>
-                            </div>
-                        ))}
+                        {items.map((item) => {
+                            const ToolIcon = item.iconType;
+                            return (
+                                <div
+                                    key={item.title}
+                                    className="inline-flex items-center gap-4 py-2 px-3 outline outline-2 outline-white/10 rounded-lg">
+                                    {/* <ToolIcon component={item.iconType} /> */}
+                                    <ToolIcon className="size-10" />
+                                    <span className="font-semibold">{item.title}</span>
+                                </div>
+                            );
+                        })}
                     </Fragment>
                 ))}
             </div>
